419b94e6fec77292b24aac8cb88950d9e6ce4e99
gentsidis <gentsidis@gmail.com>
2025-03-31 16:05:50 +0300

    v1.0.4

--- src/index.js
+++ src/index.js
@@ -1,21 +1,43 @@
+function extendConf (conf, api) {
+  // register our boot file
+  conf.boot.push('~@gentsidis/quasar-app-extension-confirm-button/src/boot/confirm-button.js')
+
+  if (api.hasWebpack) {
+    // make sure app extension files & ui package gets transpiled
+    const transpileTarget = (
+      conf.build.webpackTranspileDependencies // q/app-webpack >= v4
+      || conf.build.transpileDependencies // q/app-webpack v3
+    )
+    transpileTarget.push(/@gentsidis[\\/]quasar-app-extension-confirm-button[\\/]src/)
+  }
+
+  // make sure the stylesheet goes through webpack to avoid SSR issues
+  // conf.css.push('quasar-ui-confirm-button/src/index.sass')
+}
+
 export default function (api) {
+  // Quasar compatibility check; you may need
+  // hard dependencies, as in a minimum version of the "quasar"
+  // package or a minimum version of "@quasar/app-*" CLI
   api.compatibleWith('quasar', '^2.0.0')
 
-  api.extendQuasarConf((conf) => {
-    // Ensure framework.plugins array exists
-    conf.framework = conf.framework || {}
-    conf.framework.plugins = conf.framework.plugins || []
+  if (api.hasVite) {
+    api.compatibleWith('@quasar/app-vite', '^1.5.0 || ^2.0.0')
+  }
+  else if (api.hasWebpack) {
+    api.compatibleWith('@quasar/app-webpack', '^3.10.0 || ^4.0.0')
+  }
 
-    // Add Notify plugin if not already included
-    if (!conf.framework.plugins.includes('Notify')) {
-      conf.framework.plugins.push('Notify')
-      api.onExitLog('Added "Notify" plugin to quasar.config.js')
-    }
 
-    // Register boot file
-    conf.boot = conf.boot || []
-    conf.boot.push('confirm-button')
-  })
+  // Uncomment the line below if you provide a JSON API for your component
+  // api.registerDescribeApi('QuasarConfirmButton', '~quasar-ui-confirm-button/src/components/QuasarConfirmButton.json')
 
-  api.onExitLog('✅ ConfirmButton installed! Use <ConfirmButton> in templates.')
+
+  // We extend /quasar.conf.js
+  api.extendQuasarConf(extendConf)
+
+    api.extendQuasarConf((conf) => {
+      // Ensure Notify is automatically added when the extension is installed
+      conf.framework.plugins.push('Notify');
+    });
 }
